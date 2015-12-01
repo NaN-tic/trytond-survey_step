efa8ef0132ca37dad547388d64aac60deaf0b42c
resteve <resteve@zikzakmedia.com>
2015-12-01 01:27:11 +0100

    Tests

--- tests/test_survey_step.py
+++ tests/test_survey_step.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part of the survey module for Tryton.
+# This file is part of the survey_step module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SurveyStepCase(unittest.TestCase):
+class SurveyStepCase(ModuleTestCase):
     'Test Survey Step module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('survey_step')
-
-    def test0005views(self):
-        'Test views'
-        test_view('survey_step')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'survey_step'
 
 
 def suite():
